b957ca2e9b96c354d1b2bb3e833d556c1ae1edbd
thaerbraizat <thaerbraizat13@gmail.com>
2021-04-05 14:21:47 +0300

    add aboutme

---
+++ README.md
@@ -0,0 +1,6 @@
+# LAB - 02
+
+## ABOUT ME
+I build this website to let people know some information  about me .
+### Author : Thaer braizat
+
---
+++ js/app.js
@@ -0,0 +1,80 @@
+'use strict';
+let ageMe = prompt("do you think that iam 25 years old?")
+ageMe = ageMe.toLowerCase();
+
+switch(ageMe){
+    case 'yes':
+    case 'y':
+         console.log(ageMe);
+         alert("that's correct");
+       
+    break;
+
+    case 'no':
+    case 'n' :
+        console.log(ageMe);
+        alert("that's wrong ")
+        break;
+} 
+
+let musIc = prompt("do you think that i like music?")
+musIc = musIc.toLowerCase();
+
+switch(musIc){
+    case 'yes':
+    case 'y':
+        console.log(musIc);
+         alert("that's correct");
+    break;
+
+    case 'no':
+    case 'n' :
+        console.log(musIc);
+        alert("that's wrong ")
+        break;
+} 
+let olDer = prompt("do you think that i love computer games?")
+olDer = olDer.toLowerCase();
+switch(olDer){
+    case 'yes':
+    case 'y':
+        console.log(olDer);
+         alert("that's correct");
+    break;
+
+    case 'no':
+    case 'n' :
+        console.log(olDer);
+        alert("that's wrong ")
+        break;
+} 
+let doTa= prompt("do you think that iam dota player?")
+doTa = doTa.toLowerCase();
+switch(doTa){
+    case 'yes':
+    case 'y':
+        console.log(doTa);
+         alert("that's correct");
+    break;
+
+    case 'no':
+    case 'n' :
+        console.log(doTa);
+        alert("that's wrong ")
+        break;
+} 
+let fooTball = prompt("do you think that i like football?")
+fooTball = fooTball.toLowerCase();
+switch(fooTball){
+    case 'yes':
+    case 'y':
+        console.log(fooTball);
+         alert("that's correct");
+    break;
+
+    case 'no':
+    case 'n' :
+        console.log(fooTball);
+        alert("that's wrong ")
+        break;
+} 
